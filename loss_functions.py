761cec2f96fc80c740498a820561ae38ef5465e2
hoanghedspi <a2k40pbc@gmail.com>
2018-07-05 09:34:20 +0700

    added perceptualLoss

--- loss_functions.py
+++ loss_functions.py
@@ -12,3 +12,30 @@ class ContentLoss():
     
     def get_loss(self, realIm, fakeIm):
         return self.criterion(realIm, fakeIm)
+
+class PerceptualLoss():
+    def contentFunc(self):
+        conv_3_3_layer = 14
+        cnn = models.vgg19(pretrained=True).features
+        cnn = cnn.cuda()
+        model = nn.Sequential()
+        model = model.cuda()
+        for i, layer in enumerate(list(cnn)):
+            model.add_module(str(i), layer)
+            if i == conv_3_3_layer:
+                break
+        return model
+
+    def initialize(self, loss):
+        self.criterion = loss
+        self.contentFunc = self.contentFunc()
+
+    def get_loss(self, realIm, fakeIm):
+        f_fake = self.contentFunc.forward(fakeIm)
+        f_real = self.contentFunc.forward(realIm)
+        f_real_no_grad = f_real.detach()
+        loss = self.criterion(f_fake, f_real_no_grad)
+        return loss
+
+
+
