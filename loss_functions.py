f5a11c41f3368033f5c7abc07df9618ae3a01c92
hoanghedspi <a2k40pbc@gmail.com>
2018-07-06 09:24:08 +0700

    added GANLoss

--- loss_functions.py
+++ loss_functions.py
@@ -37,5 +37,41 @@ class PerceptualLoss():
         loss = self.criterion(f_fake, f_real_no_grad)
         return loss
 
+class GANLoss(nn.Module):
+    def __init__(self, use_l1=True, target_real_label=1.0, target_fake_label=0.0,
+            tensor=torch.FloatTensor):
+        super(GANLoss, self).__init__()
+        self.real_label = target_real_label
+        self.fake_label = target_fake_label
+        self.real_label_var = None
+        self.fake_label_var = None
+        self.Tensor = tensor
+        if use_l1:
+            self.loss = nn.L1Loss()
+        else:
+            self.loss = nn.BCELoss()
+    
+    def get_target_label(self, input, target_is_real):
+        target_tensor = None
+        if target_is_real:
+            create_label = ((self.real_label_var is None) or
+                    (self.real_label_var.numel() != input.numel()))
+            if create_label:
+                real_tensor = self.Tensor(input.size()).fill_(self.real_label)
+                self.real_label_var = Variable(real_tensor, requires_grad=False)
+            target_tensor = self.real_label_var
+        else:
+            create_label = ((self.fake_label_var is None) or 
+                    (self.fake_label_var.numel() != input.numel()))
+            if create_label:
+                fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
+                self.fake_label_var = Variable(fake_tensor, requires_grad=False)
+            target_tensor = self.fake_label_var
+        return target_tensor
+
+    def __call__(self, input, target_is_real):
+        target_tensor = self.get_target_label(input, target_is_real)
+        return self.loss(input, target_tensor)
+
 
 
