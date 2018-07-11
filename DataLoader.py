12eab81dee8b2eacb2837b78e15be0ad71584323
hoanghedspi <a2k40pbc@gmail.com>
2018-07-12 00:22:00 +0700

    added resnet block

---
+++ DataLoader.py
@@ -0,0 +1,11 @@
+import torch
+from torch.utils import data
+
+class DataLoader(data.Dataset):
+    
+    def __init__(self, ):
+    
+    def __len__(self):
+
+    def __getitem__(self, index):
+
---
+++ ResBlock.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn 
+import torchvision
+import math
+
+def conv3x3(inplanes, outplanes, stride=1):
+    return nn.Conv2d(inplanes, outplanes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+class ResBlock(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes,planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        return out
+
